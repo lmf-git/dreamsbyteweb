9ca3de31101a1e8c521c7fa98bbf3929fcb11d8f
L M Fielding <contact@lmfielding.com>
2024-05-31 17:39:52 -0300

    add meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Syne } from 'next/font/google';
 import '../components/layout/global.scss';
+import Head from 'next/head';
 
 const syne = Syne({
   weight: ['400', '500', '600', '700', '800'],
@@ -12,7 +13,9 @@ export const metadata = {
     default: 'Dreamsbyte',
   },
   description: '',
-  keywords: 'development, design, database, figma, javascript, html, css'
+  keywords: 'development, design, database, figma, javascript, html, css',
+  "msapplication-TileColor": "#ffc40d",
+  "theme-color": "#ffd800"
 };
 
 export const viewport = {
@@ -23,6 +26,13 @@ export const viewport = {
 
 export default function RootLayout({ children }) {
   return <html lang="en" className={syne.className}>
+    <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/favicons/mask-icon.svg" color="#5bbad5" />
+    </Head>
     <body>{ children }</body>
   </html>;
 };
