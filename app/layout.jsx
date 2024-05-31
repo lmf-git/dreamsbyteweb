e297ae71d06a653b5f7537ba9cd45359cc080ab7
L M Fielding <contact@lmfielding.com>
2024-05-31 17:45:17 -0300

    Add svg

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,24 +15,19 @@ export const metadata = {
   description: '',
   keywords: 'development, design, database, figma, javascript, html, css',
   "msapplication-TileColor": "#ffc40d",
-  "theme-color": "#ffd800"
+  "apple-touch-icon": "",
+  "manifest": "",
 };
 
 export const viewport = {
   width: 'device-width',
   initialScale: 1,
-  themeColor: 'transparent'
+  themeColor: '#ffd800',
+  maskIcon: "/favicons/mask-icon.svg"
 };
 
 export default function RootLayout({ children }) {
   return <html lang="en" className={syne.className}>
-    <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/favicons/mask-icon.svg" color="#5bbad5" />
-    </Head>
     <body>{ children }</body>
   </html>;
 };
