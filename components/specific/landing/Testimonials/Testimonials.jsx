98541689dec49f7c537a989d538a219689a6705d
L M Fielding <contact@lmfielding.com>
2024-07-23 21:31:33 -0300

    AI is useless.

--- components/specific/landing/Testimonials/Testimonials.jsx
+++ components/specific/landing/Testimonials/Testimonials.jsx
@@ -1,121 +1,15 @@
 'use client';
 
 import { useRef, useState } from 'react';
-
-import LVolt from '../../../icons/branding/clients/LVolt';
-
+import { testimonials } from '../../../../data.mjs';
 import styles from './testimonials.module.scss';
 
-const testimonials = [
-    {
-        logo: <LVolt />,
-        name: 'Theodore M',
-        company: 'LVolt',
-        testimonial: 'Working with DreamsByte has been an absolute game-changer for our business. They helped us not only improve our online presence but also boost our brand recognition. DreamsByte is more than a digital agency; they are partners in our success.',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Christopher K',
-        company: 'IMAA',
-        testimonial: 'Project turned out to be more complicated and include more features than we thought of at the start. Liam was very patient, helpful and flexible in incorporating many of these.',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Val A',
-        company: 'TRD',
-        testimonial: '',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: '',
-        company: 'Allied',
-        testimonial: '',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Frankie B',
-        company: '',
-        testimonial: 'Great person to work with very helpful and a great teacher',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Elliott C',
-        company: 'Relativity Labs',
-        testimonial: 'Delivered the site on a quick turnaround meeting all my requirements. When asked to make some revisions to did not hesitate to get them done. A pleasure to work with and helped me stay on schedule for my project. Very experienced with Wordpres',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Beth O',
-        company: 'Agency Project',
-        testimonial: 'Delivered the site on a quick turnaround meeting all my requirements. When asked to make some revisions to did not hesitate to get them done. A pleasure to work with and helped me stay on schedule for my project. Very experienced with Wordpres',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Dele Aden',
-        company: 'Delta3',
-        testimonial: 'Delivered the site on a quick turnaround meeting all my requirements. When asked to make some revisions to did not hesitate to get them done. A pleasure to work with and helped me stay on schedule for my project. Very experienced with Wordpres',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Andrea H',
-        company: 'Helping Hand',
-        testimonial: 'Working with Liam was excellent! He understood what I wanted and he quickly presented me with a product. He is friendly, understanding and helpful. I am so pleased I chose Liam for my web page.',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Amogh G',
-        company: 'Helping Hand',
-        testimonial: 'Really good javascript coder, and teacher. Also charges less than most. Can finish the project quickly.',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Mila E',
-        company: null,
-        testimonial: 'Working with dreamsbyte was a real pleasure. They built a gorgeous website for my small business from scratch, listened to my comments, everything works perfectly and fast, I\'m very happy (and so are my customers :)).',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Doc',
-        company: 'Electric Doctors',
-        testimonial: 'Great communication skills. They were excellent in helping us define our goals, explore available options, designing & implementing solutions. Security conscientious & driven. Superb follow up and continued services. This company has earned a client for life with us.',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Leo Lukas',
-        company: '',
-        testimonial: 'Cleanest code ever. The loading speed is amazing on each site developed. Great.',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Siddharth P',
-        company: '',
-        testimonial: '5/5',
-        reporturl: null
-    },
-    {
-        logo: null,
-        name: 'Lewis A',
-        company: '',
-        testimonial: '5/5',
-        reporturl: null
-    }
-];
+
 
 export default function Testimonials() {
     const listRef = useRef(null);
+    const planeRef = useRef(null);
+
     const [isDragging, setIsDragging] = useState(false);
     const [startX, setStartX] = useState(0);
     const [scrollLeft, setScrollLeft] = useState(0);
@@ -141,45 +35,13 @@ export default function Testimonials() {
 
     const handleTouchMove = ev => {
         if (!isDragging) return;
+        
         const x = ev.touches[0].pageX - listRef.current.offsetLeft;
         const walk = x - startX;
         listRef.current.scrollLeft = scrollLeft - walk;
     };
 
-    const stopDragging = () => {
-        if (!isDragging) return;
-        setIsDragging(false);
-        snapToTestimonial();
-    };
-
-    const snapToTestimonial = () => {
-        const testimonials = listRef.current.querySelectorAll(`.${styles.testimonial}`);
-        const scrollPosition = listRef.current.scrollLeft;
-        let closest = testimonials[0];
-        let closestDistance = Math.abs(scrollPosition - testimonials[0].offsetLeft);
-
-        testimonials.forEach(testimonial => {
-            const distance = Math.abs(scrollPosition - testimonial.offsetLeft);
-            if (distance < closestDistance) {
-                closest = testimonial;
-                closestDistance = distance;
-            }
-        });
-
-        listRef.current.scrollTo({
-            left: closest.offsetLeft,
-            behavior: 'smooth'
-        });
-    };
-
-    useEffect(() => {
-        window.addEventListener('mouseup', stopDragging);
-        window.addEventListener('touchend', stopDragging);
-        return () => {
-            window.removeEventListener('mouseup', stopDragging);
-            window.removeEventListener('touchend', stopDragging);
-        };
-    }, []);
+    const stopDragging = () => setIsDragging(false);
 
     return <div className={`section ${styles.testimonials}`} id="testimonials">
         <h2 className={styles.title}>Our client testimonials:</h2>
@@ -193,8 +55,8 @@ export default function Testimonials() {
             onTouchStart={handleTouchStart}
             onTouchMove={handleTouchMove}
             onTouchEnd={stopDragging}>
-            <div className={styles.plane}>
-                {testimonials.map((t, i) => (
+            <div className={styles.plane} ref={planeRef}>
+                { testimonials.map((t, i) => (
                     <div className={styles.testimonial} key={i}>
                         <div className={styles.brand}>
                             {t?.logo}
---
+++ data.mjs
@@ -0,0 +1,109 @@
+import LVolt from './components/icons/branding/clients/LVolt';
+
+export const testimonials = [
+    {
+        logo: <LVolt />,
+        name: 'Theodore M',
+        company: 'LVolt',
+        testimonial: 'Working with DreamsByte has been an absolute game-changer for our business. They helped us not only improve our online presence but also boost our brand recognition. DreamsByte is more than a digital agency; they are partners in our success.',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Christopher K',
+        company: 'IMAA',
+        testimonial: 'Project turned out to be more complicated and include more features than we thought of at the start. Liam was very patient, helpful and flexible in incorporating many of these.',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Val A',
+        company: 'TRD',
+        testimonial: '',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: '',
+        company: 'Allied',
+        testimonial: '',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Frankie B',
+        company: '',
+        testimonial: 'Great person to work with very helpful and a great teacher',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Elliott C',
+        company: 'Relativity Labs',
+        testimonial: 'Delivered the site on a quick turnaround meeting all my requirements. When asked to make some revisions to did not hesitate to get them done. A pleasure to work with and helped me stay on schedule for my project. Very experienced with Wordpres',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Beth O',
+        company: 'Agency Project',
+        testimonial: 'Delivered the site on a quick turnaround meeting all my requirements. When asked to make some revisions to did not hesitate to get them done. A pleasure to work with and helped me stay on schedule for my project. Very experienced with Wordpres',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Dele Aden',
+        company: 'Delta3',
+        testimonial: 'Delivered the site on a quick turnaround meeting all my requirements. When asked to make some revisions to did not hesitate to get them done. A pleasure to work with and helped me stay on schedule for my project. Very experienced with Wordpres',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Andrea H',
+        company: 'Helping Hand',
+        testimonial: 'Working with Liam was excellent! He understood what I wanted and he quickly presented me with a product. He is friendly, understanding and helpful. I am so pleased I chose Liam for my web page.',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Amogh G',
+        company: 'Helping Hand',
+        testimonial: 'Really good javascript coder, and teacher. Also charges less than most. Can finish the project quickly.',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Mila E',
+        company: null,
+        testimonial: 'Working with dreamsbyte was a real pleasure. They built a gorgeous website for my small business from scratch, listened to my comments, everything works perfectly and fast, I\'m very happy (and so are my customers :)).',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Doc',
+        company: 'Electric Doctors',
+        testimonial: 'Great communication skills. They were excellent in helping us define our goals, explore available options, designing & implementing solutions. Security conscientious & driven. Superb follow up and continued services. This company has earned a client for life with us.',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Leo Lukas',
+        company: '',
+        testimonial: 'Cleanest code ever. The loading speed is amazing on each site developed. Great.',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Siddharth P',
+        company: '',
+        testimonial: '5/5',
+        reporturl: null
+    },
+    {
+        logo: null,
+        name: 'Lewis A',
+        company: '',
+        testimonial: '5/5',
+        reporturl: null
+    }
+];
