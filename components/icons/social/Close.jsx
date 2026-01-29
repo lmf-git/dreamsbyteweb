847ca45a2a461684b5564236184a6806b0237106
lmf-git <contact@dreamsbyte.com>
2026-01-29 01:15:05 -0300

    Fix mobile menu class prop

--- components/icons/social/Close.jsx
+++ components/icons/social/Close.jsx
@@ -1,5 +1,5 @@
-export default function Close({ className }) {
-    return <svg className={className} viewBox="0 0 100 100" fill="none">
+export default function Close({ extraClass = '' }) {
+    return <svg className={extraClass} viewBox="0 0 100 100" fill="none">
         <rect x="10" y="45" width="80" height="10" rx="4" transform="rotate(45 50 50)" />
         <rect x="10" y="45" width="80" height="10" rx="4" transform="rotate(-45 50 50)" />
     </svg>;
